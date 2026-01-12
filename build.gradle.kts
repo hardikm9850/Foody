bedc9628d95a7ec9b1c97c77cb7080bbaad4ee51
Hardik.Mehta <hardik.mehta@cantire.com>
2026-01-12 15:33:42 +0530

    ci: add detekt static analysis to pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,19 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt) apply true
+}
+
+
+detekt {
+    toolVersion = libs.versions.detekt.toString()
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    parallel = true
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        txt.required.set(false)
+        sarif.required.set(true)
+    }
 }
