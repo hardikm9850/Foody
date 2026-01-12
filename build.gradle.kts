d7cfc82f53ecd57b6b61efaf5e9a66bc1119b037
Hardik.Mehta <hardik.mehta@cantire.com>
2026-01-12 17:51:05 +0530

    removed duplicate name

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ subprojects {
 detekt {
     toolVersion = libs.versions.detekt.get()
     config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
-    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
     buildUponDefaultConfig = true
     parallel = true
     reports {
